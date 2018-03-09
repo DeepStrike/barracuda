534ea7348a0baa599866248ed9960546b81b8b68
DeepStrike <drezergod@gmail.com>
2018-03-09 21:32:52 +0100

    Card block

--- blocks/line-reveal/line-reveal.js
+++ blocks/line-reveal/line-reveal.js
@@ -1,5 +1,5 @@
 var lines = document.querySelectorAll('.line-reveal__elem');
-console.log(lines);
+
 for (var i = 0; i < lines.length; i++) {
 	(function(line, time) {
 		setTimeout(function() {
--- blocks/promo/promo.js
+++ blocks/promo/promo.js
@@ -1,13 +1,3 @@
-var lines = document.querySelectorAll('.line-reveal__elem');
-
-for (var i = 0; i < lines.length; i++) {
-	(function(line, time) {
-		setTimeout(function() {
-			line.classList.add('show');
-		}, time)
-	})(lines[i], i * 500);
-}
-
 ;(function(window) {
 
   'use strict';
