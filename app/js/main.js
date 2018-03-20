f90ca3bd17d5776467c0d2b5e6b173f98652cf62
DeepStrike <drezergod@gmail.com>
2018-03-20 11:32:24 +0100

    Slider very simple

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,7 @@ for (var i = 0; i < lines.length; i++) {
 
   function init() {
     [].slice.call(document.querySelectorAll('.pagination')).forEach(function(nav) {
+
       var navItems = [].slice.call(nav.querySelectorAll('.pagination__item')),
         itemsTotal = navItems.length,
         setCurrent = function(item) {
@@ -25,16 +26,46 @@ for (var i = 0; i < lines.length; i++) {
           var currentItem = nav.querySelector('.pagination__item--current');
           currentItem.classList.remove('pagination__item--current');
           
+
           // set current
           item.classList.add('pagination__item--current');
+
+          //----------------------------------------
+          // Slider purrent
+          var slideActive = document.querySelector('.flex-active');
+          const next = item.dataset.slide;
+          const current = slideActive.dataset.slide;
+
+          if (next !== current) {
+            var el = document.querySelector('.promo__img-wrap')
+                             .querySelectorAll(`.img-inner[data-slide=\"${next}\"]`);
+
+            // el[0].classList.add('flex-preStart');
+
+            slideActive.classList.add('animate-end');
+            
+           
+            el[0].classList.remove('animate-start');
+            el[0].classList.add('flex-active');
+            slideActive.classList.add('animate-start');
+            slideActive.classList.remove('animate-end', 'flex-active');
+          }
         };
+
+
       
       navItems.forEach(function(item) {
-        item.addEventListener('click', function() { setCurrent(item); });
+        item.addEventListener('click', function(e) { 
+          setCurrent(item); }, 
+          false);
       });
+
     });
   }
 
   init();
 
 })(window);
+
+
+
--- blocks/promo/promo.js
+++ blocks/promo/promo.js
@@ -4,6 +4,7 @@
 
   function init() {
     [].slice.call(document.querySelectorAll('.pagination')).forEach(function(nav) {
+
       var navItems = [].slice.call(nav.querySelectorAll('.pagination__item')),
         itemsTotal = navItems.length,
         setCurrent = function(item) {
@@ -15,16 +16,46 @@
           var currentItem = nav.querySelector('.pagination__item--current');
           currentItem.classList.remove('pagination__item--current');
           
+
           // set current
           item.classList.add('pagination__item--current');
+
+          //----------------------------------------
+          // Slider purrent
+          var slideActive = document.querySelector('.flex-active');
+          const next = item.dataset.slide;
+          const current = slideActive.dataset.slide;
+
+          if (next !== current) {
+            var el = document.querySelector('.promo__img-wrap')
+                             .querySelectorAll(`.img-inner[data-slide=\"${next}\"]`);
+
+            // el[0].classList.add('flex-preStart');
+
+            slideActive.classList.add('animate-end');
+            
+           
+            el[0].classList.remove('animate-start');
+            el[0].classList.add('flex-active');
+            slideActive.classList.add('animate-start');
+            slideActive.classList.remove('animate-end', 'flex-active');
+          }
         };
+
+
       
       navItems.forEach(function(item) {
-        item.addEventListener('click', function() { setCurrent(item); });
+        item.addEventListener('click', function(e) { 
+          setCurrent(item); }, 
+          false);
       });
+
     });
   }
 
   init();
 
 })(window);
+
+
+
